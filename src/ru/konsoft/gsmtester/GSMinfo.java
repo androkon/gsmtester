f40b12dfad225817bdef325c1d3236993fbbf26c
Andrey Konyshkin <akonyshkin@begun.ru>
2014-09-04 20:22:59 +0400

    coding

--- src/ru/konsoft/gsmtester/GSMinfo.java
+++ src/ru/konsoft/gsmtester/GSMinfo.java
@@ -1,10 +1,9 @@
 package ru.konsoft.gsmtester;
 
-import java.util.Date;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,21 +11,33 @@ import android.os.Message;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
 
 public class GSMinfo extends Activity {
 
+	private TelephonyManager tm;
 	private static final int EXCELLENT_LEVEL = 75;
 	private static final int GOOD_LEVEL = 50;
 	private static final int MODERATE_LEVEL = 25;
 	private static final int WEAK_LEVEL = 0;
 	
-	private int lastSignalLevel = 0;
-	private Map<Long, Integer> levels;
+	private int lastSignalLevel;
 	private Timer timer;
+	
+	private LocationManager lm;
+	private String provider;
+	private Criteria criteria;
+	private Location lastLocation;
+
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +45,7 @@ public class GSMinfo extends Activity {
         setContentView(R.layout.activity_gsminfo);
         
         try{
-	        startSignalLevelListener();
+	        startAllListeners();
         }catch(Exception e){
         	setTextViewText(R.id.device_info, e.toString());
         }
@@ -69,6 +80,35 @@ public class GSMinfo extends Activity {
 		((ProgressBar) findViewById(id)).setProgress(progress);
 		setTextViewText(infoid, signalLevelString + " (" + level + ")");
 		lastSignalLevel = level;
+
+		try{
+			String simoperator = tm.getSimOperatorName();
+			String networktype = getNetworkTypeString(tm.getNetworkType());
+			String phonetype = getPhoneTypeString(tm.getPhoneType());
+			String deviceinfo = "";
+			deviceinfo += ("SIM Operator: " + simoperator + "\n");
+			deviceinfo += ("Network Type: " + networktype + "\n");
+			deviceinfo += ("Phone Type: " + phonetype + "\n");
+			setTextViewText(R.id.device_info, deviceinfo);
+        }catch(Exception e){
+        	setTextViewText(R.id.device_info, "Some errors");
+        	e.printStackTrace();
+        }
+		
+		if(lastLocation != null){
+			try{
+				String gpsinfo = "";
+				gpsinfo += "provider: " + lastLocation.getProvider() + "\n";
+				gpsinfo += "lat: " + lastLocation.getLatitude() + "\n";
+				gpsinfo += "lon: " + lastLocation.getLongitude();
+				setTextViewText(R.id.gps_info, gpsinfo);
+	        }catch(Exception e){
+	        	setTextViewText(R.id.gps_info, "Some errors");
+	        	e.printStackTrace();
+	        }
+		}else{
+			setTextViewText(R.id.gps_info, "No GPS");
+		}
 	}
 	
 	private String getSignalLevelString(int level) {
@@ -86,53 +126,63 @@ public class GSMinfo extends Activity {
 	
 	private void stopListening() {
 		
-		TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 		tm.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
 		
-		timer.cancel();
+		if(timer != null)
+			timer.cancel();
 	}
 	
-	private Handler timeHandler = new Handler() {
+	private final Handler timeHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
-			levels.put(System.currentTimeMillis(), lastSignalLevel);
+			//Data d = new  Data(lastSignalLevel);
+//			if(lastLocation == null)
+//				Log.e(getPackageName(), "last lat: null");
+//			else
+//				Log.e(getPackageName(), "last lat: " + Location.convert(lastLocation.getLatitude(), Location.FORMAT_SECONDS) + " accuracy: " + lm.getProvider(provider).getAccuracy());
 			super.handleMessage(msg);
 		}
 	};	
 	
-	private void startSignalLevelListener() {
-		TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+	@SuppressLint("UseSparseArrays") private void startAllListeners() {
+		// get GSM mobile network settings
+		tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 		int events = PhoneStateListener.LISTEN_SIGNAL_STRENGTHS;
 		tm.listen(phoneStateListener, events);
+		lastSignalLevel = 0;
 		
-		timer = new Timer();
+		// get GPS coordinates
+		lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+		criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		criteria.setCostAllowed(false);
+		provider = lm.getBestProvider(criteria, false);
+		//Log.e(getPackageName(), "prov: " + provider);
+		lastLocation = null;
+		//Log.e(getPackageName(), "lat: " + Location.convert(lastLocation.getLatitude(), Location.FORMAT_SECONDS));
+		// location updates: at least 1 meter and 1 secs change
+		lm.requestLocationUpdates(provider, 1000, 1, gpsLocationListener);
+
+		// create collection data timer
+		timer = new  Timer();
 		long delay = 1000, interval = 1000;
 		timer.schedule(new TimerTask() {
 			@Override
 			public void run() {
+				//Log.e(getPackageName(), "timertask");
 				timeHandler.obtainMessage().sendToTarget();
 			}
 		}, delay, interval);
 	}
 	
-	private void displayTelephonyInfo() {
-		TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-		String simoperator = tm.getSimOperatorName();
-		String networktype = getNetworkTypeString(tm.getNetworkType());
-		String phonetype = getPhoneTypeString(tm.getPhoneType());
-		String deviceinfo = "";
-		deviceinfo += ("SIM Operator: " + simoperator + "\n");
-		deviceinfo += ("Network Type: " + networktype + "\n");
-		deviceinfo += ("Phone Type: " + phonetype + "\n");
-		setTextViewText(R.id.device_info, deviceinfo);
-	}
-
 	private final PhoneStateListener phoneStateListener = new PhoneStateListener() {
 
 		@Override
 		public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+			
 			setSignalLevel(R.id.signalLevel, R.id.signalLevelInfo, signalStrength.getGsmSignalStrength());
-			displayTelephonyInfo();
+			//Log.e(getPackageName(), "gsm: " + signalStrength.getGsmSignalStrength());
+			
 			super.onSignalStrengthsChanged(signalStrength);
 		}
 	
@@ -170,6 +220,32 @@ public class GSMinfo extends Activity {
 		}
 	}
 
+	private final LocationListener gpsLocationListener = new LocationListener() {
+		
+		@Override
+		public void onLocationChanged(Location location) {
+			lastLocation = location;
+			//Log.e(getPackageName(), "change lat: " + Location.convert(lastLocation.getLatitude(), Location.FORMAT_SECONDS));
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			//Log.e(getPackageName(), "gps: onProviderEnabled");
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			Log.e(getPackageName(), "gps: onProviderDisabled");
+			lastLocation = null;
+		}
+		
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			//Log.e(getPackageName(), "gps: onStatusChanged");
+		}
+
+	};
+
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -192,4 +268,5 @@ public class GSMinfo extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
